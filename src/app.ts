ae87e879493da4f58d4e52c4462b5bf4ca75e47f
Ronald Assis <ronaldassis71@gmail.com>
2022-04-25 16:42:45 -0300

    feat: login and callback router

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
-import express from 'express'
+import 'dotenv/config';
+import express from 'express';
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`:rocket Running at http;//localhost:${PORT}`));
+app.get('/login', (_req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+})
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query;
+
+  return res.json(code);
+})
+
+app.listen(PORT, () => console.log(`:rocket Running at http://localhost:${PORT}`));
